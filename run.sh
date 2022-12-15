e7880f28aae560f13509f8b99ea2513a3864f9b0
Nagy-Abdallah <120485592+Nagy-Abdallah@users.noreply.github.com>
2022-12-16 00:14:55 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "VagrantUP"
+#vagrant up
+vagrant ssh master  << EOF
+  ls ;
+  git clone https://github.com/Nagy-Abdallah/Provision-catDogApp.git
+  ls
+  cd Provision-catDogApp
+  kubectl create ns vote
+  kubectl apply -f k8s-manifests -n vote
+  kubectl get pods -n vote
+EOF
