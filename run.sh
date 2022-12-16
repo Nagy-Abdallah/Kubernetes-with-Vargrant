3424436dc71800bd86bafb609b2f5370f4c36ce4
Nagy-Abdallah <120485592+Nagy-Abdallah@users.noreply.github.com>
2022-12-16 23:07:46 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
+echo "changing to vagrant folder"
 cd vagrant
-echo "VagrantUP"
-#vagrant up
-vagrant ssh master  << EOF
-  ls ;
-  git clone https://github.com/Nagy-Abdallah/Provision-catDogApp.git
+echo "RunningVagrantUP"
+vagrant up
+vagrant ssh master  << EOF    #startting from here will run inside the Master-node
+  echo "inside Master-Node"
+  echo "running git clone"
+  git clone https://github.com/Nagy-Abdallah/Provision-catDogApp.git 
+  echo "listing "
   ls
+  echo "changing to Provision-catDogApp folder"
   cd Provision-catDogApp
+  echo "Applying the application manifests files from the cloned repo"
   kubectl create ns vote
   kubectl apply -f k8s-manifests -n vote
   kubectl get pods -n vote
